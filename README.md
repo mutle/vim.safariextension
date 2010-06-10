3f232bc7941ffb835f838765ef905831ff60b286
Mutwin Kraus <mutwin.kraus@gmail.com>
2010-06-10 09:43:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Vim Keybindings for Safari
+--------------------------
+
+Currently Supported:
+
+* gg, G
+* h, j, k, l
+* ^D ^U
---
+++ vim-keybindings.js
@@ -0,0 +1,58 @@
+var keycombo = '';
+
+window.document.addEventListener("keydown", function(e) {
+  var t = {}, c = String.fromCharCode(e.keyCode).toLowerCase(), SCROLL_STEP = 35;
+
+  t.scroll = function(x, y) {
+    window.scrollBy(x, y);
+  };
+  t.scrollTo = function(x, y) {
+    window.scrollTo(x, y);
+  };
+
+  t.halfWindowHeight = function() {
+    return window.innerHeight / 2;
+  };
+
+  t.screenHeight = function() {
+    return document.body.offsetHeight;
+  };
+
+  if(e.shiftKey) c = c.toUpperCase();
+
+  switch(c) {
+  case 'g':
+    if(keycombo == 'g')
+      t.scrollTo(0, 0);
+    else {
+      keycombo = c;
+      window.setTimeout(function() { keycombo = ''; }, 500);
+    }
+  break;
+  case 'h':
+    t.scroll(-SCROLL_STEP, 0);
+  break;
+  case 'j':
+    t.scroll(0, SCROLL_STEP);
+  break;
+  case 'k':
+    t.scroll(0, -SCROLL_STEP);
+  break;
+  case 'l':
+    t.scroll(SCROLL_STEP, 0);
+  break;
+  case 'd':
+    if(e.ctrlKey) {
+      t.scroll(0, t.halfWindowHeight());
+    }
+  break;
+  case 'u':
+    if(e.ctrlKey) {
+      t.scroll(0, -t.halfWindowHeight());
+    }
+  break;
+  case 'G':
+    t.scrollTo(0, t.screenHeight());
+  break;
+  }
+});
