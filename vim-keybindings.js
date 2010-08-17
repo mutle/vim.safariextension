24d23e6d09e05ad27254676840ec72101fbd4fd9
Jannik Nielsen <jannik@liseogjannik.dk>
2010-08-17 17:12:29 +0200

    Moved the t object to global scope Created a function for the double tap that was inside the g function. Created a lastActiveElement inside the t object that holds a reference to the element that was active when esc was pressed Added i key to enter insert mode (the last active element gains focus) Added dd key to remove the content og the last active element Added all function/meta keys to the ctrl+d key combo, to make sure to only react on ctrl+d

--- vim-keybindings.js
+++ vim-keybindings.js
@@ -1,10 +1,13 @@
 var keycombo = '';
+var oldkey = '';
+var t = {};
 
 window.document.addEventListener("keydown", function(e) {
-  var t = {}, c = String.fromCharCode(e.keyCode).toLowerCase(), SCROLL_STEP = 35;
+  var c = String.fromCharCode(e.keyCode).toLowerCase(), SCROLL_STEP = 35;
 
   if(window.document.activeElement !== window.document.body) {
     if (e.keyCode == 27) {
+	  t.lastActiveElement = window.document.activeElement;
       window.document.activeElement.blur();
     }
     return;
@@ -21,24 +24,30 @@ window.document.addEventListener("keydown", function(e) {
     return window.innerHeight / 2;
   };
 
-  t.fullWindowHeight = function() {
-    return window.innerHeight;
-  };
-
   t.screenHeight = function() {
     return document.body.offsetHeight;
   };
+
+  t.doubleTap = function(key) {
+	if (oldkey == '') {
+	  oldkey = key;
+	  window.setTimeout(function() { oldkey = ''; }, 500);
+	  return false;
+	}
+	else {
+	  if (oldkey == key) {
+		oldkey = '';
+		return true;
+	  }
+	}
+  }
 
   if(e.shiftKey) c = c.toUpperCase();
 
   switch(c) {
   case 'g':
-    if(keycombo == 'g')
+    if(t.doubleTap('g'))
       t.scrollTo(0, 0);
-    else {
-      keycombo = c;
-      window.setTimeout(function() { keycombo = ''; }, 500);
-    }
   break;
   case 'h':
     t.scroll(-SCROLL_STEP, 0);
@@ -53,27 +62,39 @@ window.document.addEventListener("keydown", function(e) {
     t.scroll(SCROLL_STEP, 0);
   break;
   case 'd':
-    if(e.ctrlKey) {
+    if(e.ctrlKey && !e.metaKey && !e.altKey && !e.shiftKey && !e.altGraphKey) {
       t.scroll(0, t.halfWindowHeight());
-    }
+    } else if (t.doubleTap('d')) {
+		if (t.lastActiveElement != undefined) {
+			t.lastActiveElement.value = '';
+		}
+	}
   break;
   case 'f':
-    if(e.ctrlKey) {
+    if(e.ctrlKey && !e.metaKey && !e.altKey && !e.shiftKey && !e.altGraphKey) {
       t.scroll(0, t.fullWindowHeight());
     }
   break;
+  
   case 'u':
     if(e.ctrlKey) {
       t.scroll(0, -t.halfWindowHeight());
     }
   break;
   case 'b':
-    if(e.ctrlKey) {
+    if(e.ctrlKey && !e.metaKey && !e.altKey && !e.shiftKey && !e.altGraphKey) {
       t.scroll(0, -t.fullWindowHeight());
     }
   break;
   case 'G':
     t.scrollTo(0, t.screenHeight());
   break;
-  }
+  case 'i':
+	if (t.lastActiveElement != undefined) {
+		t.lastActiveElement.focus();
+		e.preventDefault();
+	}
+	break;
+}
 });
+
